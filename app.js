e6ec3dd0ece626df055a1b90574a7bd73c0ce221
Lucia Sanchez <92888197+lucia-sanchez@users.noreply.github.com>
2023-01-12 10:06:50 -0300

    agregué html de formularios

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //rutas
 app.get('/', (req,res)=> res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/login', (req,res)=> res.sendFile(path.resolve(__dirname,'views','login.html')));
+app.get('/register', (req,res)=> res.sendFile(path.resolve(__dirname,'views','register.html')));
 
 //servidor
 app.listen(port, ()=> console.log(`Servidor corriendo en http://localhost:${port}`));
